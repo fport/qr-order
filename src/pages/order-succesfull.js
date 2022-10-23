52178e561dc075f698effd8ab64de700b75d9555
fport <furkanportakalx@gmail.com>
2022-10-23 12:18:48 +0300

    feat: style order-succesfull page

--- src/pages/order-succesfull.js
+++ src/pages/order-succesfull.js
@@ -1,8 +1,14 @@
 import Basic from "@layout/basic"
+import { TiTick } from 'react-icons/ti'
 
 const OrderSuccesfull = () => {
     return (
-        <Basic>OrderSuccesfull</Basic>
+        <Basic>
+            <div className="h-full w-full flex flex-col items-center justify-center text-green-600">
+                <div><TiTick className="text-9xl" /></div>
+                Siparişiniz Verildi
+            </div>
+        </Basic>
     )
 }
 
